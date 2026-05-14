5c44af190feba4350280c9b32865fecd3d38468e
ResofireV2 <stryguardian@gmail.com>
2026-05-14 13:46:04 -0500

    Add files via upload

--- assets/js/admin/AdminPwaPanel.jsx
+++ assets/js/admin/AdminPwaPanel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { api } from "../lib/api";
 import { fmtBytes } from "../lib/utils";
 import { toast } from "../components/Toasts";
