ec4952ab4284a6c139e631476f2537b005212a03
ResofireV2 <stryguardian@gmail.com>
2026-06-04 21:24:08 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nexus.MixProject do
   def project do
     [
       app: :nexus,
-      version: "0.3.6-beta",
+      version: "0.3.7-beta",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
